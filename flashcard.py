f4cca254ea0b1292d79952f9272701d4fd246349
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-10-26 17:29:23 +0600

    ghcfgbth

---
+++ flashcard.py
@@ -0,0 +1,32 @@
+class flashcard:
+    
+    def __init__(self, word, meaning):
+        self.word = word
+        self.meaning = meaning
+        
+    def __str__(self):
+        return f"{self.word} (  {self.meaning} )"
+    
+    
+flash = []
+print("Welcome to flashcard application")
+
+while(True):
+    
+    word = input("Enter the name you want to add to flashcard:")
+    meaning = input("Enter the meaning of the word:")
+    
+    
+    flash.append( flashcard(word, meaning))
+    
+    option = int(input("Enter 0, if you want to add another flashcard otherwise enter 1:"))
+    
+    
+    if(option):
+        break
+    
+    
+    
+print("\nYour flashcards")
+for i in flash:
+    print(">", i)
